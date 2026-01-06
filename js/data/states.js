298847d3e3001ace835f835af256def477faf3e6
DipolarBear <sunnyalexliang@gmail.com>
2026-01-05 21:53:58 -0800

    Create states.js
    Complete state/province lists for US, CA, AU, MX

---
+++ js/data/states.js
@@ -0,0 +1,25 @@
+export const STATES = {
+  US: [
+    "AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA",
+    "HI","ID","IL","IN","IA","KS","KY","LA","ME","MD",
+    "MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ",
+    "NM","NY","NC","ND","OH","OK","OR","PA","RI","SC",
+    "SD","TN","TX","UT","VT","VA","WA","WV","WI","WY",
+    "DC"
+  ],
+
+  CA: [
+    "AB","BC","MB","NB","NL","NS","NT","NU","ON","PE",
+    "QC","SK","YT"
+  ],
+
+  AU: [
+    "NS","VI","QL","SA","WA","TA","NT","AC"
+  ],
+
+  MX: [
+    "AG","BC","BS","CM","CS","CH","CO","DG","GT","GR",
+    "HG","JC","EM","MI","MO","NA","NL","OA","PU","QE",
+    "QR","SL","SI","SO","TB","TL","VE","YU","ZA"
+  ]
+};
