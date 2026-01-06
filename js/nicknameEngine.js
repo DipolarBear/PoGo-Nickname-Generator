e66803a57396dab843c1c9174c9441f974b4e61e
DipolarBear <sunnyalexliang@gmail.com>
2026-01-05 21:27:34 -0800

    Update nicknameEngine.js
    Added content to nicknameEngine.js

--- js/nicknameEngine.js
+++ js/nicknameEngine.js
@@ -1,3 +1,17 @@
-export function generateNickname(inputs) {
-  return "";
+export function generateNickname(input) {
+  /*
+    input = {
+      isPokeGenie: boolean,
+      locality: string | null,        // local friends only
+      country: string | null,         // PG only
+      state: string | null,           // PG only
+      friendDate: { y, m, d },
+      bestFriendDate: { y, m, d } | null,
+      foreverFriendDate: { y, m } | null,
+      status: string                  // "", "X", "XY"
+    }
+  */
+
+  // TEMP: stub output so we can test wiring
+  return "TEST_OK";
 }
